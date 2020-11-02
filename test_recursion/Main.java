1c4c978a45f1b46b73643ce9d6535b973e7fb454
Vinicius Sales <viniciusc.doom@gmail.com>
2020-11-02 19:46:07 -0300

    Update Main.java

--- test_recursion/Main.java
+++ test_recursion/Main.java
@@ -1,28 +1,25 @@
 package com.company;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
 
-    static void towerOfHanoi(int n, char hasteOrigem, char hasteMover, char hasteAuxiliar) {
+    static void torreInfo(int n, char hasteOrigem, char hasteMover, char hasteAuxiliar) {
         if (n == 1) {
-            System.out.println("Movendo disco 1 da haste " +  hasteOrigem + " para a haste " + hasteMover);
+            System.out.println("[disco 1] da haste " +  hasteOrigem + " para a haste " + hasteMover);
             return;
         }
 
-        towerOfHanoi(n-1, hasteOrigem, hasteAuxiliar, hasteMover);
-        System.out.println("Movendo disco " + n + " da haste " +  hasteOrigem + " para a haste " + hasteMover);
-        towerOfHanoi(n-1, hasteAuxiliar, hasteMover, hasteOrigem);
+        torreInfo(n-1, hasteOrigem, hasteAuxiliar, hasteMover);
+        System.out.println("[disco " + n + "] da haste " +  hasteOrigem + " para a haste " + hasteMover);
+        torreInfo(n-1, hasteAuxiliar, hasteMover, hasteOrigem);
     }
 
-    //  Driver method
     public static void main(String args[]) {
         Scanner discos = new Scanner(System.in);
         System.out.println("Digite quantidade de discos: ");
         Integer numeroDiscos = discos.nextInt();
-
-        int n = numeroDiscos; // Number of disks
-        towerOfHanoi(n, 'A', 'C', 'B');
+        int n = numeroDiscos;
+        torreInfo(n, 'A', 'C', 'B');
     }
 }
 
